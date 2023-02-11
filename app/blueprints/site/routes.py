0f1d849a5e4baa47f4af6d63be2ea4dbb499f5a0
JosephSalinas <75646269+JosephSalinas@users.noreply.github.com>
2023-02-11 13:38:44 -0500

    Clean up
    - Removed Pycache, updated .gitignore
    - Added dependencies to requirements.txt

--- app/blueprints/site/routes.py
+++ app/blueprints/site/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
+import urllib.request, json
 
 site = Blueprint('site', __name__, template_folder='app/templates')
 
 @site.route('/')
-def home():
+def landing_page():
     return render_template('index.html')
