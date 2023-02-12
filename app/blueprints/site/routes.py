97f17090cc0546b415688d5af14a185d9c5f189a
JosephSalinas <75646269+JosephSalinas@users.noreply.github.com>
2023-02-11 22:03:46 -0500

    Added HTMLForm

--- app/blueprints/site/routes.py
+++ app/blueprints/site/routes.py
@@ -1,8 +1,33 @@
-from flask import Blueprint, render_template
-import urllib.request, json
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+import requests 
 
 site = Blueprint('site', __name__, template_folder='app/templates')
 
-@site.route('/')
+@site.route('/', methods=["POST", "GET"])
 def landing_page():
+    '''
+    if request.method == 'POST':
+        form_data = request.form
+        
+        url = '/numbers'
+        response = request.post(url, json=form_data)
+        
+        if response.status_code == 200:
+            response_data = response.json()
+            
+            # Issue the GET request to the same API
+            get_response = request.get(url)
+            if get_response.status_code == 200:
+                get_response_data = get_response.json()
+                # Process the response data as needed
+                # ...
+                
+                # Return the processed data to the client as part of the response
+                return jsonify(get_response_data)
+            else:
+                return 'GET request failed', get_response.status_code
+        else:
+            return 'POST request failed', response.status_code
+    else:
+        '''
     return render_template('index.html')
