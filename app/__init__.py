90ab1acfd866d89f27e03735e4b3572a08642377
JosephSalinas <75646269+JosephSalinas@users.noreply.github.com>
2023-02-12 08:23:45 -0500

    Stuff

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from .blueprints.site.routes import site
 
 def create_app():
     app = Flask(__name__)
-    
     app.register_blueprint(site)
-
+    
     return app
--- app/blueprints/site/routes.py
+++ app/blueprints/site/routes.py
@@ -1,33 +1,44 @@
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-import requests 
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify, current_app
+import requests, boto3, botocore, os, time
 
 site = Blueprint('site', __name__, template_folder='app/templates')
 
-@site.route('/', methods=["POST", "GET"])
+@site.route('/')
 def landing_page():
-    '''
-    if request.method == 'POST':
-        form_data = request.form
-        
-        url = '/numbers'
-        response = request.post(url, json=form_data)
-        
-        if response.status_code == 200:
-            response_data = response.json()
-            
-            # Issue the GET request to the same API
-            get_response = request.get(url)
-            if get_response.status_code == 200:
-                get_response_data = get_response.json()
-                # Process the response data as needed
-                # ...
-                
-                # Return the processed data to the client as part of the response
-                return jsonify(get_response_data)
-            else:
-                return 'GET request failed', get_response.status_code
-        else:
-            return 'POST request failed', response.status_code
-    else:
-        '''
+    return render_template('index.html')
+
+@site.route('/form', methods=["GET"])
+def form_handler():
+    if request.method == 'GET':
+
+        session = boto3.Session(
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'),
+            region_name='us-east-1'
+        )
+
+        #s3 = session.client('s3')
+        #s3.upload_fileobj(video, 'formfusion', 'video.mp4')
+        #s3.upload_fileobj(pro_video, 'formfusion', 'pro_video.mp4')
+
+        s3 = session.client('s3')
+        s3.upload_file('./app/static/images/video.mp4', 'formfusion', 'video.mp4')
+        s3.upload_file('./app/static/images/pro_video.mp4', 'formfusion', 'pro_video.mp4')
+
+        url = 'https://6df4-2600-1000-b01e-5091-dc3f-30cd-9218-358e.ngrok.io/formfusion'
+
+        selected_joints = []
+        for k, v in request.args.items():
+            selected_joints.append(int(k))
+
+        response = requests.post(url, json={'numbers': selected_joints})
+        print(selected_joints)
+        print(response.status_code)
+        assert response.status_code == 200
+
+        time.sleep(15)
+
+        s3.download_file('formfusion', 'annotated_video.mp4', './app/static/images/annotated_video.mp4')
+        s3.download_file('formfusion', 'annotated_pro_video.mp4', './app/static/images/annotated_pro_video.mp4')
+
     return render_template('index.html')
