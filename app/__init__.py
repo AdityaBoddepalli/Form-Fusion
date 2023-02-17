ccab10d629dc480e252dc7ce946b7fbdc928f655
Joseph Salinas <75646269+JosephSalinas@users.noreply.github.com>
2023-02-17 09:33:27 -0500

    Delete app folder
    - Deleting app folder to push new implementation

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-from .blueprints.site.routes import site
-
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(site)
-    
-    return app
--- app/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, redirect, url_for, render_template
-from app import create_app
-
-# create the app object
-app = create_app()
-
-if  __name__ == "__main__":
-    app.run(debug=True)
